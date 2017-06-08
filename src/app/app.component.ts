cea3e52ceb7d98a91f3093a40319386bb130bf18
Apel <apel.kun@gmail.com>
2017-06-09 02:42:38 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import '../assets/scss/styles.scss';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public angularLogo = 'assets/images/angular.png';
+};
